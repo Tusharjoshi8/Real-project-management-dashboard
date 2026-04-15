80de780377db82c0a91be0056dcad500b4728b32
Tushar Joshi <tusharjoshi2385@gmail.com>
2026-04-15 22:08:08 +0530

    api updated again

--- my-project/src/api.js
+++ my-project/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://real-time-4o86.onrender.com';
+const API_BASE_URL = 'https://real-project-backend.onrender.com/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
